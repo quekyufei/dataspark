6479c8fb62c0178b09db96d8cf194377368df942
Ernest <erntye@hotmail.com>
2018-02-25 03:23:49 +0800

    commit

---
+++ dataspark/flask_website/ADD STUFF HERE/possible_use_cases/routeplanner.py
@@ -0,0 +1,22 @@
+import googlemaps as gmp
+import polyline
+
+API_KEY = "AIzaSyAKc0dmMyEXyn_R2Kp5q24Ba54DjXawTXA"
+gmap = gmp.Client(key=API_KEY)
+origin = (1.31859, 103.84658)
+destination = (1.32626, 103.85567)
+
+
+def get_poly_line(origin, destination):
+    result = gmap.directions(origin= origin,
+        destination=destination, mode="transit", transit_mode="train")
+    if result:
+        polyline_list = polyline.decode(result[0]["overview_polyline"]['points'])
+        polyline_dict = ",\n".join(
+            ["{{lat: {lat}, lng: {lon}}}".format(lat=x[0], lon=x[1]) for x in polyline_list])
+        print(polyline_dict)
+    else:
+        print("trip not valid")
+
+
+print(get_poly_line(origin, destination))
