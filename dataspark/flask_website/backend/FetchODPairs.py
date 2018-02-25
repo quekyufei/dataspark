f994d6a6eaf6185a5e4a68be66781156aa457da0
quekyufei <quekyufei@hotmail.com>
2018-02-25 09:15:45 +0800

    adbjshdasbhjdasjbhafj a

--- dataspark/flask_website/backend/FetchODPairs.py
+++ dataspark/flask_website/backend/FetchODPairs.py
@@ -2,86 +2,90 @@ import requests
 import time
 import json
 import operator
-from apiconnnection import APIConnection as Conn
-import definequerybody
+import os
+
+from .apiconnnection import APIConnection as Conn
+from . import definequerybody
 
 
 # The main function in the class, an example is below
 # Input is a dict example: {"gender": "NA", "age": [1990, 1995], "race": "NA", "nationality": "NA"}
 # Output
 def fetchODPairs(filterdict):
-    subzoneDict = {}
-    with open("subzones1.txt", "r") as file:
-        for line in file:
-            (key, val) = line.split('\t')
-            subzoneDict[key] = val.replace("\n", "")
+	subzoneDict = {}
+	__location__ = os.path.realpath(
+	os.path.join(os.getcwd(), os.path.dirname(__file__)))
+	with open(os.path.join(__location__,"subzones1.txt"), "r") as file:
+		for line in file:
+			(key, val) = line.split('\t')
+			subzoneDict[key] = val.replace("\n", "")
 
-    subzoneRank = {}
+	subzoneRank = {}
 
-    subzoneNum = 1
+	subzoneNum = 1
 
-    for i in subzoneDict:
+	for i in subzoneDict:
 
-        if subzoneNum % 76 == 0:
-            time.sleep(60)
+		if subzoneNum % 76 == 0:
+			time.sleep(60)
 
-        queryBody = {
-            "date": "2018-02-14",
-            "timeSeriesReference": "destination",
-            "location": {
-                "locationType": "locationHierarchyLevel",
-                "levelType": "destination_subzone",
-                "id": i
-            },
-            "queryGranularity": {
-                "type": "period",
-                "period": "P1D"
-            },
-            "dimensionFacets": [
-                "origin_subzone"
-            ],
-            "aggregations": [
-                {
-                    "metric": "unique_agents",
-                    "type": "hyperUnique"
-                }
-            ]
-        }
+		queryBody = {
+			"date": "2018-02-14",
+			"timeSeriesReference": "destination",
+			"location": {
+				"locationType": "locationHierarchyLevel",
+				"levelType": "destination_subzone",
+				"id": i
+			},
+			"queryGranularity": {
+				"type": "period",
+				"period": "P1D"
+			},
+			"dimensionFacets": [
+				"origin_subzone"
+			],
+			"aggregations": [
+				{
+					"metric": "unique_agents",
+					"type": "hyperUnique"
+				}
+			]
+		}
 
-        queryBody.update(definequerybody.main_filter_thing_OD(filterdict, i))
+		queryBody.update(definequerybody.main_filter_thing_OD(filterdict, i))
 
-        queryResponse = requests.post("https://apistore.datasparkanalytics.com:443/odmatrix/v3/query",
-                                      data=json.dumps(queryBody),
-                                      headers={'Authorization': "Bearer " + Conn.token,
-                                               'Content-Type': Conn.contentType}
-                                      )
-        result = queryResponse.json()
+		queryResponse = requests.post("https://apistore.datasparkanalytics.com:443/odmatrix/v3/query",
+									  data=json.dumps(queryBody),
+									  headers={'Authorization': "Bearer " + Conn.token,
+											   'Content-Type': Conn.contentType}
+									  )
+		result = queryResponse.json()
 
-        print(str(subzoneNum) + ": dest->[" + str(i) + "]")
-        subzoneNum += 1
+		print(str(subzoneNum) + ": dest->[" + str(i) + "]")
+		subzoneNum += 1
 
-        # for k in range(0, len(result)):
-        #     string = str(result[k]['event']['origin_subzone']) + ": " \
-        #              + str(result[k]['event']['hyperUnique_unique_agents'])
-        #     print(string)
+		# for k in range(0, len(result)):
+		#     string = str(result[k]['event']['origin_subzone']) + ": " \
+		#              + str(result[k]['event']['hyperUnique_unique_agents'])
+		#     print(string)
 
-        sorted_result = sorted(result, key=lambda subzone: subzone['event']['hyperUnique_unique_agents'], reverse=True)
+		sorted_result = sorted(result, key=lambda subzone: subzone['event']['hyperUnique_unique_agents'], reverse=True)
 
-        for k in range(0, 3):
-            tempStr = str(sorted_result[k]['event']['origin_subzone']) + "->" + str(i) + "->" + str(
-                sorted_result[k]['timestamp'])
-            subzoneRank[tempStr] = int(sorted_result[k]['event']['hyperUnique_unique_agents'])
+		for k in range(0, 3):
+			tempStr = str(sorted_result[k]['event']['origin_subzone']) + "->" + str(i) + "->" + str(
+				sorted_result[k]['timestamp'])
+			subzoneRank[tempStr] = int(sorted_result[k]['event']['hyperUnique_unique_agents'])
 
-        for area in subzoneRank:
-            print(str(area) + "\t" + str(subzoneRank[str(area)]))
+		for area in subzoneRank:
+			print(str(area) + "\t" + str(subzoneRank[str(area)]))
 
-        print("__________________")
+		print("__________________")
 
-    sorted_x = sorted(subzoneRank.items(), key=operator.itemgetter(1), reverse=True)
-    for y in range(0,3):
-        print(sorted_x[y])
+	sorted_x = sorted(subzoneRank.items(), key=operator.itemgetter(1), reverse=True)
+	for y in range(0,3):
+		print(sorted_x[y])
 
-    return sorted_x[0:3]
+	return sorted_x[0:3]
 
 
 # For testing purposes!
--- dataspark/flask_website/backend/get_subzone.py
+++ dataspark/flask_website/backend/get_subzone.py
@@ -1,18 +1,18 @@
 import os 
 
 def get_subzone(subzone):
-    subzone_dict = {}
-    __location__ = os.path.realpath(
-    os.path.join(os.getcwd(), os.path.dirname(__file__)))
+		subzone_dict = {}
+		__location__ = os.path.realpath(
+		os.path.join(os.getcwd(), os.path.dirname(__file__)))
 
-    with open(os.path.join(__location__, "subzones.txt"), "r") as file:
-        for line in file:
-            (key, val) = line.split('\t')
-            subzone_dict[key] = val.replace("\n", "")
-    if subzone_dict[subzone]:
-        return subzone_dict[subzone]
-    else:
-        return 'Null'
+		with open(os.path.join(__location__, "subzones.txt"), "r") as file:
+				for line in file:
+						(key, val) = line.split('\t')
+						subzone_dict[key] = val.replace("\n", "")
+		if subzone_dict[subzone]:
+				return subzone_dict[subzone]
+		else:
+				return 'Null'
 
 #print(get_subzone('GLSZ04'))
 
